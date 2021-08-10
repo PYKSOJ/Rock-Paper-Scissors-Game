88535364c1f579e5b026f338b6201770802baa37
PYKSOJ <62101102166@psu.ac.th>
2021-08-10 20:19:34 +0700

    update gitignore && reinitial

---
+++ play.py
@@ -0,0 +1,24 @@
+def play(player):
+    # m = ["r", "p", "s"]
+    d = {"r": "rock", "p": "paper", "s": "scissors"}
+    # computer = m[random.randrange(len(m))]
+    computer = random.choice(list(d.keys()))
+    if player == computer:
+        result = "tie"
+    elif player == "r":
+        if computer == "s":
+            result = "player win"
+        else:
+            result = "computer win"
+    elif player  == "p":
+        if computer == "r":
+            result = "player win"
+        else:
+            result = "computer win"
+    elif player == "s":
+        if computer == "p":
+            result = "player win"
+        else:
+            result = "computer win"
+    # print("Player choose ", d[player], "and computer choose", d[computer], "and result is ", result)
+    print("player choose {} and computer choose {} and result is {}". format(d[player], d[computer], result))
--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,30 +1,5 @@
 import random
-
-def play(player):
-    # m = ["r", "p", "s"]
-    d = {"r": "rock", "p": "paper", "s": "scissors"}
-    # computer = m[random.randrange(len(m))]
-    computer = random.choice(list(d.keys()))
-    if player == computer:
-        result = "tie"
-    elif player == "r":
-        if computer == "s":
-            result = "player win"
-        else:
-            result = "computer win"
-    elif player  == "p":
-        if computer == "r":
-            result = "player win"
-        else:
-            result = "computer win"
-    elif player == "s":
-        if computer == "p":
-            result = "player win"
-        else:
-            result = "computer win"
-    # print("Player choose ", d[player], "and computer choose", d[computer], "and result is ", result)
-    print("player choose {} and computer choose {} and result is {}". format(d[player], d[computer], result))
-
+from play import play
 while True:
     player = input("[r]ock, [p]aper, [s]cissors \nwhat U chose :")
     if player != "r" and player != "p" and player != "s" :
